df53a566d1353d7a3c707c21ec36d903271de8cd
Divyansh <divyansh9111@gmail.com>
2023-05-20 17:57:41 +0530

    host11

--- backend/app.js
+++ backend/app.js
@@ -29,17 +29,17 @@ app.use(function (req, res, next) {
 app.use(errorHandler);
 
 // middleware
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Request-Headers', '*');
-  if (req.method === "OPTIONS") {
-    res.header('Access-Control-Allow-Methods', '*');
-    return res.status(200).json({});
-  }
-  next();
-});
-
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', '*');
+//   res.header('Access-Control-Request-Headers', '*');
+//   if (req.method === "OPTIONS") {
+//     res.header('Access-Control-Allow-Methods', '*');
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
+app.use(cors());
 // ***************************production********************************
 const _dirname1 = path.resolve();
 
